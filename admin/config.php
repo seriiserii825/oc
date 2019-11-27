fca337cdb648262549cf65d15ef23f74b108688e
serii <seriiburduja@gmail.com>
2019-11-27 22:47:35 +0200

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.host1670806.hostland.pro/admin/');
+define('HTTP_CATALOG', 'http://oc.host1670806.hostland.pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.host1670806.hostland.pro/admin/');
+define('HTTPS_CATALOG', 'http://oc.host1670806.hostland.pro/');
+
+// DIR
+define('DIR_APPLICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/admin/');
+define('DIR_SYSTEM', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/');
+define('DIR_IMAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/image/');
+define('DIR_LANGUAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/admin/language/');
+define('DIR_TEMPLATE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/admin/view/template/');
+define('DIR_CONFIG', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/config/');
+define('DIR_CACHE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/download/');
+define('DIR_LOGS', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/modification/');
+define('DIR_UPLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/upload/');
+define('DIR_CATALOG', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'host1670806_0106');
+define('DB_PASSWORD', 'OT6zBw6k');
+define('DB_DATABASE', 'host1670806_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.host1670806.hostland.pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.host1670806.hostland.pro/');
+
+// DIR
+define('DIR_APPLICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/catalog/');
+define('DIR_SYSTEM', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/');
+define('DIR_IMAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/image/');
+define('DIR_LANGUAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/catalog/language/');
+define('DIR_TEMPLATE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/catalog/view/theme/');
+define('DIR_CONFIG', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/config/');
+define('DIR_CACHE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/download/');
+define('DIR_LOGS', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/modification/');
+define('DIR_UPLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'host1670806_0106');
+define('DB_PASSWORD', 'OT6zBw6k');
+define('DB_DATABASE', 'host1670806_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
